e854d3310f99e319ce2313d49b53c193dfd8ff7a
Dmitry Khrapov <hrapovd@ya.ru>
2021-01-24 21:05:19 +0300

    write skeleton of program

--- cmd/sys-mon/main.go
+++ cmd/sys-mon/main.go
@@ -3,36 +3,42 @@ package main
 import (
 	"flag"
 	"fmt"
-	//	"github.com/hrapovd1/final_project/pkg/sysmon"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	sysmon "github.com/hrapovd1/final_project/pkg/sysmon"
 )
 
 func main() {
 	// Program flags
-	isDaemon := flag.Bool("d", false, "daemon mode")
 	isHelp := flag.Bool("h", false, "help output")
 	dataBuff := flag.Uint("b", 5, "average data period in sec")
 	port := flag.Uint("p", 8080, "server default tcp port")
 	answPeriod := flag.Uint("a", 5, "server answer period in sec")
 	flag.Parse()
-
+	// Print help message and exit.
 	if *isHelp {
-		fmt.Println("sys-mon [-d] [-b n] [-a m] [-p PORT]\n\t-d background mode\n\t-p PORT server port to listen clients\n\t-b n data buffer in seconds\n\t-a m answer period to client every m seconds\n\t-h this help")
+		fmt.Println("sys-mon [-b n] [-a m] [-p PORT]\n\t-p PORT server port to listen clients\n\t-b n data buffer in seconds\n\t-a m answer period to client every m seconds\n\t-h this help")
 		return
 	}
-	if *isDaemon {
-		fmt.Println("Program will be run in background.")
-	} else {
-		fmt.Println("Program will be run in foreground.")
+
+	// Subscribe os signals
+	sysSigCh := make(chan os.Signal, 1)
+	signal.Notify(sysSigCh, syscall.SIGINT, syscall.SIGTERM)
+	// Setup logger to stdout
+	stdoutLog := log.New(os.Stdout, "", log.LstdFlags)
+
+	// Create new sys-mon
+	monInstance := sysmon.NewSysmon(*dataBuff, *answPeriod, *port)
+	err := monInst.Run(stdoutLog, sysSigCh)
+	if err != nil {
+		stdoutLog.Printf("ERROR: %v", err)
 	}
-	if *port < 1 {
-		fmt.Println("port must be from 1 to 65535")
-	} else {
-		fmt.Printf("Server open %v port\n", *port)
-	}
-	if *dataBuff > 0 {
-		fmt.Printf("wait %v seconds to get statistics\n", *dataBuff)
-	}
-	if *answPeriod > 0 {
-		fmt.Printf("next answer through %v seconds\n", *answPeriod)
-	}
+
+	<-sysSigCh
+	stdoutLog.Println("Got stop signal")
+	syscall.Exit(0)
 }
--- pkg/sys-mon/sys-mon.go
+++ pkg/sys-mon/sys-mon.go
@@ -1,1 +1,134 @@
 package sysmon
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type Sysmon interface {
+	Run(logger log.Logger, sysCh <-chan os.Signal) error
+}
+
+type sysmonState struct {
+	port       uint
+	dataBuff   uint
+	answPeriod uint
+}
+
+var smState *sysmonState
+
+type listner struct {
+	cmd   string
+	pid   uint
+	user  string
+	proto uint
+	port  uint
+}
+
+type monState struct {
+	loadAver   uint
+	cpu        [3]uint
+	diskLoad   [2]uint
+	fsUsage    map[string][2]uint
+	netListner listner
+	netSocks   uint
+}
+
+func NewSysmon(dataBuff, answPeriod, port *uint) Sysmon {
+	smState = &sysmonState{
+		port:       &port,
+		dataBuff:   &dataBuff,
+		answPeriod: &answPeriod,
+	}
+	return &monState{
+		loadAver:   0,
+		cpu:        {0, 0, 0},
+		diskLoad:   {0, 0},
+		fsUsage:    make(map[string][2]uint, 1),
+		netListner: make(listner),
+		netSocks:   0,
+	}
+}
+
+func (mst *monState) Run(logger log.Logger, sysCh <-chan os.Signal) error {
+	go func(sysCh <-chan os.Signal) {
+		select {
+		case <-sysCh:
+			return
+		default:
+			for {
+				time.Sleep(time.Duration(*smState.dataBuff) * time.Second)
+				logger.Println("run load data getter")
+			}
+		}
+	}(sysCh)
+	go func(sysCh <-chan os.Signal) {
+		select {
+		case <-sysCh:
+			return
+		default:
+			for {
+				time.Sleep(time.Duration(*smState.dataBuff) * time.Second)
+				logger.Println("run cpu data getter")
+			}
+		}
+	}(sysCh)
+	go func(sysCh <-chan os.Signal) {
+		select {
+		case <-sysCh:
+			return
+		default:
+			for {
+				time.Sleep(time.Duration(*smState.dataBuff) * time.Second)
+				logger.Println("run disk data getter")
+			}
+		}
+	}(sysCh)
+	go func(sysCh <-chan os.Signal) {
+		select {
+		case <-sysCh:
+			return
+		default:
+			for {
+				time.Sleep(time.Duration(*smState.dataBuff) * time.Second)
+				logger.Println("run fs data getter")
+			}
+		}
+	}(sysCh)
+	go func(sysCh <-chan os.Signal) {
+		select {
+		case <-sysCh:
+			return
+		default:
+			for {
+				time.Sleep(time.Duration(*smState.dataBuff) * time.Second)
+				logger.Println("run netstat data getter")
+			}
+		}
+	}(sysCh)
+	go func(sysCh <-chan os.Signal) {
+		select {
+		case <-sysCh:
+			return
+		default:
+			for {
+				time.Sleep(time.Duration(*smState.dataBuff) * time.Second)
+				logger.Println("run data aggregator")
+			}
+		}
+	}(sysCh)
+	go func(sysCh <-chan os.Signal) {
+		select {
+		case <-sysCh:
+			return
+		default:
+			for {
+				time.Sleep(time.Duration(*smState.answPeriod) * time.Second)
+				logger.Printf("open %v port to listen...", &smState.port)
+			}
+		}
+	}(sysCh)
+
+	return nil
+}
