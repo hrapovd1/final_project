254117d4edd6a8a4361a5c94183bc39ae9d7c2bc
Dmitry Khrapov <hrapovd@ya.ru>
2021-01-24 22:02:32 +0300

    rename pakage dir

--- cmd/sys-mon/main.go
+++ cmd/sys-mon/main.go
@@ -8,7 +8,7 @@ import (
 	"os/signal"
 	"syscall"
 
-	sysmon "github.com/hrapovd1/final_project/pkg/sys-mon"
+	sysmon "github.com/hrapovd1/final_project/pkg/sysmon"
 )
 
 func main() {
@@ -18,6 +18,7 @@ func main() {
 	port := flag.Uint("p", 8080, "server default tcp port")
 	answPeriod := flag.Uint("a", 5, "server answer period in sec")
 	flag.Parse()
+
 	// Print help message and exit.
 	if *isHelp {
 		fmt.Println("sys-mon [-b n] [-a m] [-p PORT]\n\t-p PORT server port to listen clients\n\t-b n data buffer in seconds\n\t-a m answer period to client every m seconds\n\t-h this help")
@@ -27,10 +28,11 @@ func main() {
 	// Subscribe os signals
 	sysSigCh := make(chan os.Signal, 1)
 	signal.Notify(sysSigCh, syscall.SIGINT, syscall.SIGTERM)
+
 	// Setup logger to stdout
 	stdoutLog := log.New(os.Stdout, "", log.LstdFlags)
 
-	// Create new sys-mon
+	// Create and run sys-mon
 	monInstance := sysmon.NewSysmon(*dataBuff, *answPeriod, *port)
 	err := monInstance.Run(*stdoutLog, sysSigCh)
 	if err != nil {
