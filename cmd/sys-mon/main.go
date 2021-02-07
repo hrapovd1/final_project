5fd5acc3595a5cfebd1faddd40e232ffa0f4de22
Dmitry Khrapov <hrapovd@ya.ru>
2021-02-07 21:49:04 +0300

    MVP, worked. It get and send loadAverage and cpu load data

--- cmd/sys-mon/main.go
+++ cmd/sys-mon/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -13,17 +12,11 @@ import (
 
 func main() {
 	// Program flags
-	isHelp := flag.Bool("h", false, "help output")
 	dataBuff := flag.Uint("b", 5, "average data period in sec")
 	port := flag.Uint("p", 8080, "server default tcp port")
 	answPeriod := flag.Uint("a", 5, "server answer period in sec")
-	flag.Parse()
 
-	// Print help message and exit.
-	if *isHelp {
-		fmt.Println("sys-mon [-b n] [-a m] [-p PORT]\n\t-p PORT server port to listen clients\n\t-b n data buffer in seconds\n\t-a m answer period to client every m seconds\n\t-h this help")
-		return
-	}
+	flag.Parse()
 
 	// Subscribe os signals
 	sysSigCh := make(chan os.Signal, 1)
@@ -46,6 +39,4 @@ func main() {
 	<-sysSigCh
 	close(doneCh)
 	stdoutLog.Println("Got stop signal")
-
-	syscall.Exit(0)
 }
